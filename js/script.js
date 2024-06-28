330a2e9b7df477559c70a3865f3a0aaee8d26759
neslondiaze <neslondiaze@gmail.com>
2024-06-27 22:31:16 -0400

    Styles: Estilo para el Cubo ND

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Variables para controlar el estado de arrastre y las posiciones iniciales
+let isDragging = false;
+let startX, startY;
+let currentX = 0,
+  currentY = 0;
+
+// Obtiene el elemento del cubo por su ID
+const cube = document.getElementById("cube");
+
+//Evento para iniciar el arrastre
+
+//Evento para controlar el movimiento del ratón durante el arrastre
+
+//Evento para finalizar el arrastre
