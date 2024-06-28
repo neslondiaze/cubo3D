91dc9f6e5efb749f889a3d337ab8413f5f855135
neslondiaze <neslondiaze@gmail.com>
2024-06-27 23:12:26 -0400

    Code: definicion y movimiento del cubo X Y ND

--- js/script.js
+++ js/script.js
@@ -8,7 +8,28 @@ let currentX = 0,
 const cube = document.getElementById("cube");
 
 //Evento para iniciar el arrastre
+cube.addEventListener("mousedown", (e) => {
+  isDragging = true;
+  startX = e.clientX;
+  startY = e.clientY;
+  cube.style.cursor = "grabbing";
+});
 
 //Evento para controlar el movimiento del ratón durante el arrastre
+document.addEventListener("mousemove", (e) => {
+  if (isDragging) {
+    const deltaX = e.clientX - startX;
+    const deltaY = e.clientY - startY;
+    currentX += deltaX;
+    currentY -= deltaY;
+    cube.style.transform = `rotateX(${currentY}deg) rotateY(${currentX}deg)`;
+    startX = e.clientX;
+    startY = e.clientY;
+  }
+});
 
 //Evento para finalizar el arrastre
+document.addEventListener("mouseup", () => {
+  isDragging = false;
+  cube.style.cursor = "grab";
+});
